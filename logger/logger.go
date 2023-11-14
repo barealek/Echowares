ad64120089a80e3af740a29c6364cd12c4b73cec
Alek <92643775+barealek@users.noreply.github.com>
2023-11-14 21:29:13 +0100

    Create logger.go

---
+++ logger/logger.go
@@ -0,0 +1,86 @@
+package echologger
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sync/atomic"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+func New(config ...EchoLoggerConfig) echo.MiddlewareFunc {
+	cfg := configDefault(config...)
+	if strings.Contains(cfg.Format, "${latency}") {
+		cfg.enableLatency = true
+	}
+
+	cfg.timeZoneLocation = time.Local
+	cfg.tags = findTags(cfg.Format)
+
+	var timestamp atomic.Value
+	timestamp.Store(time.Now().Format(cfg.TimeFormat))
+
+	// Update time every 250 milliseconds in a seperate goroutine
+	if strings.Contains(cfg.Format, TagTime) {
+		go func() {
+			for {
+				time.Sleep(250 * time.Millisecond)
+				timestamp.Store(time.Now().Format(cfg.TimeFormat))
+			}
+		}()
+	}
+
+	// Get PID of current process
+	pid := strconv.Itoa(os.Getpid())
+
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+
+			var start, end time.Time
+
+			if cfg.enableLatency {
+				start = time.Now()
+			}
+
+			next(c)
+
+			if cfg.enableLatency {
+				end = time.Now()
+			}
+
+			tagsToReplace := map[string]string{}
+			fmt.Printf("cfg.tags: %v\n", cfg.tags)
+
+			for _, tag := range cfg.tags {
+				switch tag {
+				case TagPid:
+					tagsToReplace[tag] = pid
+				case TagLatency:
+					tagsToReplace[tag] = end.Sub(start).String()
+				case TagTime:
+					tagsToReplace[tag] = timestamp.Load().(string)
+				case TagStatus:
+					tagsToReplace[tag] = strconv.Itoa(c.Response().Status)
+				case TagMethod:
+					tagsToReplace[tag] = c.Request().Method
+				case TagPath:
+					tagsToReplace[tag] = c.Request().URL.Path
+				case TagHost:
+					tagsToReplace[tag] = c.Request().Host
+				}
+			}
+
+			fmt.Printf("tagsToReplace: %v\n", tagsToReplace)
+
+			log := replaceAll(cfg.Format, tagsToReplace)
+
+			cfg.output.Printf(log)
+
+			return nil
+		}
+
+	}
+}
