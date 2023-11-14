34bb80b4649516322c97fcb4d8d290001d2ebdc2
Alek <92643775+barealek@users.noreply.github.com>
2023-11-14 22:40:58 +0100

    Update constants

--- logger/constants.go
+++ logger/constants.go
@@ -8,14 +8,14 @@ var (
 	TagMethod  = "${method}"
 	TagHost    = "${host}"
 	TagPath    = "${path}"
+	TagError   = "${error}"
 
-	spaceLatency = 12
-	spaceTime    = 8  // 15:04:05 = 8 chars
-	spacePid     = 5  // 65535 = 5 chars
-	spaceStatus  = 3  // 200, 404, 500 = 3 chars
-	spaceMethod  = 6  // DELETE = 6 chars
-	spaceHost    = 15 // 100.100.100.100 = 15 chars
-	spacePath    = 12
-
-	ErrorPadding = 15
+	spaceLatency          = 12
+	spaceTime             = 8  // 15:04:05 = 8 chars
+	spacePid              = 5  // 65535 = 5 chars
+	spaceStatus           = 3  // 200, 404, 500 = 3 chars
+	spaceMethod           = 6  // DELETE = 6 chars
+	spaceHost             = 15 // 100.100.100.100 = 15 chars
+	spacePath             = 12
+	absoluteErrorPosition = 75 // The error will be placed at the 50th character, or at the end of the line if the format is longer than 50 characters
 )
