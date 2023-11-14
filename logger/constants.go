573bf20860120d10a52d1c6e945e7b30b6d0f3cb
Alek <92643775+barealek@users.noreply.github.com>
2023-11-14 21:28:11 +0100

    Add a few more constants

--- logger/constants.go
+++ logger/constants.go
@@ -3,7 +3,19 @@ package echologger
 var (
 	TagLatency = "${latency}"
 	TagTime    = "${time}"
+	TagPid     = "${pid}"
 	TagStatus  = "${status}"
 	TagMethod  = "${method}"
+	TagHost    = "${host}"
 	TagPath    = "${path}"
+
+	spaceLatency = 12
+	spaceTime    = 8  // 15:04:05 = 8 chars
+	spacePid     = 5  // 65535 = 5 chars
+	spaceStatus  = 3  // 200, 404, 500 = 3 chars
+	spaceMethod  = 6  // DELETE = 6 chars
+	spaceHost    = 15 // 100.100.100.100 = 15 chars
+	spacePath    = 12
+
+	ErrorPadding = 15
 )
