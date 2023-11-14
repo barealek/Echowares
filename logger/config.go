19e573ec9fbb2a47cb132907ae6bb7b445c4433b
Alek <92643775+barealek@users.noreply.github.com>
2023-11-14 20:34:20 +0100

    Update config.go

--- logger/config.go
+++ logger/config.go
@@ -1,27 +1,34 @@
 package echologger
 
 import (
+	"fmt"
 	"io"
+	"strings"
+	"time"
 
 	"github.com/fatih/color"
 )
 
 type EchoLoggerConfig struct {
-	Format string `yaml:"format"`
-
+	Format     string `yaml:"format"`
 	TimeFormat string `yaml:"time_format"`
 
 	DisableColors bool `yaml:"colors"`
 
-	Output io.Writer
-
+	Output  io.Writer
 	colorer *color.Color
+
+	TimeZone         string `yaml:"time_zone"`
+	timeZoneLocation *time.Location
+
+	enableLatency bool
 }
 
 var (
 	DefaultEchoLoggerConfig = EchoLoggerConfig{
-		Format:        "method=${method}, uri=${uri}, status=${status}\n",
-		TimeFormat:    "dd:MM:yyyy hh:mm:ss",
+		Format:        fmt.Sprintf("[%v] %v - %v %v %v\n", TagTime, TagStatus, TagLatency, TagMethod, TagPath),
+		TimeFormat:    "15:04:05",
+		TimeZone:      "Local",
 		DisableColors: true,
 		Output:        color.Output,
 		colorer:       color.New(),
@@ -49,6 +56,17 @@ func configDefault(config ...EchoLoggerConfig) EchoLoggerConfig {
 
 	if cfg.colorer == nil {
 		cfg.colorer = DefaultEchoLoggerConfig.colorer
+	}
+
+	if strings.Contains(cfg.Format, "${latency}") {
+		cfg.enableLatency = true
+	}
+
+	tz, err := time.LoadLocation(cfg.TimeZone)
+	if err != nil || tz == nil {
+		cfg.timeZoneLocation = time.Local
+	} else {
+		cfg.timeZoneLocation = tz
 	}
 
 	return cfg
