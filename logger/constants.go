1e858f7250a5abdddae1580dc052a8af547869ca
Alek <92643775+barealek@users.noreply.github.com>
2023-11-15 15:54:37 +0100

    Update constants.go

--- logger/constants.go
+++ logger/constants.go
@@ -10,12 +10,12 @@ var (
 	TagPath    = "${path}"
 	TagError   = "${error}"
 
-	spaceLatency          = 8
+	spaceLatency          = 10 // 2.202771ms = 10 chars (the longest duration in Go on the Linux operating system)
 	spaceTime             = 8  // 15:04:05 = 8 chars
 	spacePid              = 5  // 65535 = 5 chars
 	spaceStatus           = 3  // 200, 404, 500 = 3 chars
 	spaceMethod           = 6  // DELETE = 6 chars
 	spaceHost             = 15 // 100.100.100.100 = 15 chars
 	spacePath             = 12
-	absoluteErrorPosition = 75 // The error will be placed at the 50th character, or at the end of the line if the format is longer than 50 characters
+	absoluteErrorPosition = 74 // The error will be placed at the 50th character, or at the end of the line if the format is longer than 50 characters
 )
